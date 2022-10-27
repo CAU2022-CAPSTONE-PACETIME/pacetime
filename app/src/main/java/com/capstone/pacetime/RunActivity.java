48b67b042e7348408f92e44fe457c416caa3ec20
girinssh <tngus5851@naver.com>
2022-10-28 06:39:02 +0900

    Create RunActivity

---
+++ app/src/main/java/com/capstone/pacetime/RunActivity.java
@@ -0,0 +1,15 @@
+package com.capstone.pacetime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RunActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_run);
+
+    }
+}
