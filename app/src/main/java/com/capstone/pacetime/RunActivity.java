e3f383f950d84aeb62736c1ba7ecb56501e2ce1e
girinssh <tngus5851@naver.com>
2022-10-29 14:00:42 +0900

    Add DataBinding Adapt viewModel

--- app/src/main/java/com/capstone/pacetime/RunActivity.java
+++ app/src/main/java/com/capstone/pacetime/RunActivity.java
@@ -1,15 +1,30 @@
 package com.capstone.pacetime;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.capstone.pacetime.command.RunDetailInfoUpdateCommand;
+import com.capstone.pacetime.command.RunInfoUpdateCommand;
+import com.capstone.pacetime.databinding.ActivityRunBinding;
+import com.capstone.pacetime.viewmodel.RunDetailInfoViewModel;
+
 public class RunActivity extends AppCompatActivity {
+    private ActivityRunBinding binding;
+    private RunDetailInfoViewModel viewModel;
+    private RunInfoUpdateCommand command;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_run);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_run);
+        viewModel = new RunDetailInfoViewModel();
+        command = new RunDetailInfoUpdateCommand();
+
+        command.setViewModel(viewModel);
+        binding.setDetailRunInfo(viewModel);
 
     }
 }
