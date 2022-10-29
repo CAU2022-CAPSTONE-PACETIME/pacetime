51e90b1cd3270d12ad2c5c9653ebcf11a23d196f
girinssh <tngus5851@naver.com>
2022-10-29 17:49:43 +0900

    Add Temp implementation code for GPS

--- app/src/main/java/com/capstone/pacetime/RunActivity.java
+++ app/src/main/java/com/capstone/pacetime/RunActivity.java
@@ -1,20 +1,41 @@
 package com.capstone.pacetime;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.databinding.DataBindingUtil;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Message;
+import android.os.Parcelable;
+import android.util.Log;
 
 import com.capstone.pacetime.command.RunDetailInfoUpdateCommand;
 import com.capstone.pacetime.command.RunInfoUpdateCommand;
 import com.capstone.pacetime.databinding.ActivityRunBinding;
+import com.capstone.pacetime.receiver.GPSReceiver;
 import com.capstone.pacetime.viewmodel.RunDetailInfoViewModel;
+import com.google.android.gms.location.CurrentLocationRequest;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.CancellationToken;
+import com.google.android.gms.tasks.CancellationTokenSource;
+import com.google.android.gms.tasks.OnTokenCanceledListener;
 
 public class RunActivity extends AppCompatActivity {
     private ActivityRunBinding binding;
     private RunDetailInfoViewModel viewModel;
     private RunInfoUpdateCommand command;
 
+    private FusedLocationProviderClient client;
+    private GPSReceiver gps;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +46,58 @@ public class RunActivity extends AppCompatActivity {
 
         command.setViewModel(viewModel);
         binding.setDetailRunInfo(viewModel);
+        client = LocationServices.getFusedLocationProviderClient(this);
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            ActivityResultLauncher<String[]> locationPermissionRequest =
+                    registerForActivityResult(new ActivityResultContracts
+                                    .RequestMultiplePermissions(), result -> {
+                                Boolean fineLocationGranted = result.getOrDefault(
+                                        Manifest.permission.ACCESS_FINE_LOCATION, false);
+                                Boolean coarseLocationGranted = result.getOrDefault(
+                                        Manifest.permission.ACCESS_COARSE_LOCATION,false);
+                                if (fineLocationGranted != null && fineLocationGranted) {
+                                    // Precise location access granted.
+                                } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                                    // Only approximate location access granted.
+                                } else {
+                                    // No location access granted.
+                                }
+                            }
+                    );
+
+            locationPermissionRequest.launch(new String[] {
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            });
+        }
+        HandlerThread thread = new HandlerThread("GPSHandlerThread");
+        thread.start();
+        Handler handler = new Handler(thread.getLooper()){
+            @Override
+            public void handleMessage(Message message){
+                Bundle data = message.getData();
+                if(data == null){
+                    return;
+                }
+                if(data.keySet().contains("location")){
+                    Location loc = data.getParcelable("location");
+
+                    Log.i("RunActivity", "GPS: " + loc.toString());
+                }
+            }
+        };
+
+        gps = new GPSReceiver(client, handler);
+        gps.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gps.stop();
     }
 }
