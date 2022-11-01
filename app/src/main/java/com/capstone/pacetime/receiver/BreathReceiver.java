c1f33c7fef8c2f56a1d6d685d4089e69af302719
girinssh <tngus5851@naver.com>
2022-11-01 20:23:15 +0900

    Temporary code

--- app/src/main/java/com/capstone/pacetime/receiver/BreathReceiver.java
+++ app/src/main/java/com/capstone/pacetime/receiver/BreathReceiver.java
@@ -1,6 +1,45 @@
 package com.capstone.pacetime.receiver;
 
-public class BreathReceiver {
+import android.Manifest;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.media.AudioManager;
 
+public class BreathReceiver implements StartStopInterface, SensorEventListener {
+    private AudioManager audioManager;
+    private StartStopInterface command;
 
+    public static String[] PERMISSIONS = new String[]{
+            Manifest.permission.RECORD_AUDIO,
+    };
+
+    public BreathReceiver(AudioManager audioManager){
+        this.audioManager = audioManager;
+    }
+
+    public void setCommand(StartStopInterface command){
+
+        this.command = command;
+    }
+
+    @Override
+    public void start() {
+        command.start();
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
 }
