1963cbaa5466ad97639bb349dad27ec9bf269dfa
girinssh <tngus5851@naver.com>
2022-11-01 20:26:15 +0900

    Add getLocation method

--- app/src/main/java/com/capstone/pacetime/receiver/GPSReceiver.java
+++ app/src/main/java/com/capstone/pacetime/receiver/GPSReceiver.java
@@ -5,18 +5,12 @@ import android.annotation.SuppressLint;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.HandlerThread;
 import android.os.Message;
-import android.renderscript.RenderScript;
-
-import androidx.annotation.NonNull;
+import android.util.Log;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.Priority;
 import com.google.android.gms.tasks.CancellationTokenSource;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnTokenCanceledListener;
-import com.google.android.gms.tasks.Task;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -24,7 +18,7 @@ import java.util.TimerTask;
 public class GPSReceiver implements StartStopInterface{
     private final Handler dataHandler;
     private final Timer receiveTimer;
-    private final TimerTask receiveTask;
+    private TimerTask receiveTask;
 
     private final FusedLocationProviderClient client;
 
@@ -34,40 +28,48 @@ public class GPSReceiver implements StartStopInterface{
                 Manifest.permission.ACCESS_BACKGROUND_LOCATION
     };
 
-    @SuppressLint("MissingPermission")
     public GPSReceiver(FusedLocationProviderClient client, Handler dataHandler) {
         this.client = client;
         this.dataHandler = dataHandler;
         this.receiveTimer = new Timer();
-        this.receiveTask = new TimerTask() {
-            @Override
-            public void run() {
-                CancellationTokenSource receiverCts = new CancellationTokenSource();
-                client.getCurrentLocation(Priority.PRIORITY_BALANCED_POWER_ACCURACY, receiverCts.getToken())
-                        .addOnSuccessListener(
-                                (location) -> {
-                                    if (location != null) {
-                                        Bundle loc = new Bundle();
-                                        loc.putParcelable("location", location);
-                                        Message message = new Message();
-                                        message.setData(loc);
-                                        dataHandler.sendMessage(message);
-                                    }
-                                }
-                        );
-
-            }
-        };
-
     }
     @Override
     public void start(){
+        this.receiveTask = new ReceiveTask();
         receiveTimer.schedule(receiveTask, 0, 1000);
+        Log.i("GPSReceiver", "STARTED");
     }
     @Override
     public void stop(){
         receiveTask.cancel();
-        receiveTimer.cancel();
-        receiveTimer.purge();
+        receiveTask = null;
+
+        Log.i("GPSReceiver", "STOPPED");
     }
+
+    public void getLocation(){
+        this.receiveTask = new ReceiveTask();
+        receiveTimer.schedule(receiveTask, 0);
+    }
+
+    private class ReceiveTask extends TimerTask{
+        @SuppressLint("MissingPermission")
+        @Override
+        public void run() {
+            CancellationTokenSource receiverCts = new CancellationTokenSource();
+            client.getCurrentLocation(Priority.PRIORITY_BALANCED_POWER_ACCURACY, receiverCts.getToken())
+                    .addOnSuccessListener(
+                            (location) -> {
+                                if (location != null) {
+                                    Bundle loc = new Bundle();
+                                    loc.putParcelable("location", location);
+                                    Message message = new Message();
+                                    message.setData(loc);
+                                    dataHandler.sendMessage(message);
+                                }
+                            }
+                    );
+        }
+    }
+
 }
