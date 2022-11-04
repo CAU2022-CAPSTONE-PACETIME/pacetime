71ab33a01e72b6ed0358a4cf39e87051fd7c14cf
girinssh <tngus5851@naver.com>
2022-11-04 21:48:20 +0900

    create DataThread

--- app/src/main/java/com/capstone/pacetime/RunningManager.java
+++ app/src/main/java/com/capstone/pacetime/RunningManager.java
@@ -1,5 +1,6 @@
 package com.capstone.pacetime;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.hardware.SensorManager;
 import android.location.Location;
@@ -13,54 +14,103 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.capstone.pacetime.data.Breath;
+import com.capstone.pacetime.data.Step;
 import com.capstone.pacetime.receiver.BreathReceiver;
 import com.capstone.pacetime.receiver.GPSReceiver;
 import com.capstone.pacetime.receiver.StartStopInterface;
 import com.capstone.pacetime.receiver.StepCounter;
 import com.google.android.gms.location.LocationServices;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class RunningManager implements StartStopInterface {
     private final GPSReceiver gpsReceiver;
     private final BreathReceiver breathReceiver;
     private final StepCounter stepCounter;
 
-    private RunInfo runInfo;
+    private UpdateTask updateTask;
+    private final Timer updateTimer;
+
+    private final RunInfo runInfo;
     private RunningState state;
 
     private Handler handler;
     private final HandlerThread thread ;
 
-    public RunningManager(AppCompatActivity activity){
-        thread = new HandlerThread("DataHandlerThread");
-        thread.start();
+    class DataThread extends HandlerThread{
+
+        public DataThread(String name) {
+            super(name);
+        }
+
+        @Override
+        public synchronized void start() {
+            super.start();
+            handler = new Handler(getLooper()) {
+                @Override
+                public void handleMessage(@NonNull Message msg) {
+                    super.handleMessage(msg);
+                    if(msg.arg1 == RunningDataType.BREATH.ordinal()){
+                        runInfo.addBreathItem((Breath)msg.obj);
+                    }
+                    else if(msg.arg1 == RunningDataType.LOCATION.ordinal()){
+                        Bundle data = msg.getData();
+                        if(data == null){
+                            return ;
+                        }
+                        Location loc = data.getParcelable("location");
+                        Log.i("RunningManager", "GPS: " + loc.toString());
+
+                        runInfo.addTrace(loc);
+
+                    }else if(msg.arg1 == RunningDataType.STEP.ordinal()){
+                        runInfo.addStepCount((Step)msg.obj);
+                        breathReceiver.doConvert(System.currentTimeMillis());
+                    }
+                }
+            };
+
+            gpsReceiver.setDataHandler(handler);
+            breathReceiver.setDataHandler(handler);
+            stepCounter.setDataHandler(handler);
+        }
+    }
+
+    public RunningManager(AppCompatActivity activity, RunInfo runInfo){
+        this.runInfo = runInfo;
+
+        updateTimer = new Timer();
+
+        thread = new DataThread("DataHandlerThread");
 
         SensorManager sensorManager = (SensorManager) activity.getApplicationContext().getSystemService(Context.SENSOR_SERVICE);
 
-        gpsReceiver = new GPSReceiver(LocationServices.getFusedLocationProviderClient(activity), handler);
-        breathReceiver = new BreathReceiver((AudioManager) activity.getApplicationContext().getSystemService(Context.AUDIO_SERVICE), handler);
-        stepCounter = new StepCounter(sensorManager, handler);
+        gpsReceiver = new GPSReceiver(LocationServices.getFusedLocationProviderClient(activity));
+        breathReceiver = new BreathReceiver((AudioManager) activity.getApplicationContext().getSystemService(Context.AUDIO_SERVICE));
+        stepCounter = new StepCounter(sensorManager);
+    }
+
+    public void changeState(RunningState state){
+        this.state = state;
+    }
+
+
+    private class UpdateTask extends TimerTask {
+        @SuppressLint("MissingPermission")
+        @Override
+        public void run() {
+            runInfo.update();
+        }
     }
 
     @Override
     public void start(){
-        handler = new Handler(thread.getLooper(), (@NonNull Message msg) -> {
-                if(msg.arg1 == RunningDataType.BREATH.ordinal()){
+        thread.start();
+        this.updateTask = new UpdateTask();
+        updateTimer.schedule(updateTask, 1000, 1000);
 
-                }
-                else if(msg.arg1 == RunningDataType.LOCATION.ordinal()){
-                    Bundle data = msg.getData();
-                    if(data == null){
-                        return false;
-                    }
-                    Location loc = data.getParcelable("location");
-
-                    Log.i("RunningManager", "GPS: " + loc.toString());
-                }else if(msg.arg1 == RunningDataType.STEP.ordinal()){
-                    breathReceiver.doConvert(System.currentTimeMillis());
-                }
-                return true;
-            }
-        );
         gpsReceiver.start();
         breathReceiver.start();
         stepCounter.start();
@@ -68,6 +118,9 @@ public class RunningManager implements StartStopInterface {
 
     @Override
     public void stop() {
+        updateTask.cancel();
+        updateTask = null;
+
         gpsReceiver.stop();
         breathReceiver.stop();
         stepCounter.stop();
@@ -75,8 +128,8 @@ public class RunningManager implements StartStopInterface {
 
     public static String[][] getPermissionSets(){
         return new String[][]{
-                GPSReceiver.PERMISSIONS,
                 BreathReceiver.PERMISSIONS,
+                GPSReceiver.PERMISSIONS,
                 StepCounter.PERMISSIONS
         };
     }
