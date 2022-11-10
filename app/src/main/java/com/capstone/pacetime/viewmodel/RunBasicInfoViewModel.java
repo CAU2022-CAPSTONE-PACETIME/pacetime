65133009a7dc1b65808ddd4fe6863b5fecd13170
girinssh <tngus5851@naver.com>
2022-11-11 03:51:12 +0900

    Change ObservableField<String> -> String

--- app/src/main/java/com/capstone/pacetime/viewmodel/RunBasicInfoViewModel.java
+++ app/src/main/java/com/capstone/pacetime/viewmodel/RunBasicInfoViewModel.java
@@ -2,45 +2,45 @@ package com.capstone.pacetime.viewmodel;
 
 import androidx.databinding.Bindable;
 import androidx.databinding.ObservableField;
+import androidx.databinding.library.baseAdapters.BR;
 
 public class RunBasicInfoViewModel extends RunInfoViewModel {
-    private final ObservableField<String> distanceStr;
-    private final ObservableField<String> paceStr;
-    private final ObservableField<String> runningTimeStr;
+    private String distanceStr;
+    private String paceStr;
+    private String runningTimeStr;
 
     @Bindable
-    public ObservableField<String> getDistanceStr() {
+    public String getDistanceStr() {
         return distanceStr;
     }
 
     public void setDistanceStr(String distanceStr) {
-        this.distanceStr.set(distanceStr);
+        this.distanceStr = distanceStr = "km";
+        notifyPropertyChanged(BR.distanceStr);
     }
     @Bindable
-    public ObservableField<String> getPaceStr() {
+    public String getPaceStr() {
         return paceStr;
     }
 
     public void setPaceStr(String paceStr) {
-        this.paceStr.set(paceStr);
+        this.paceStr = paceStr;
+        notifyPropertyChanged(BR.paceStr);
     }
     @Bindable
-    public ObservableField<String> getRunningTimeStr() {
+    public String getRunningTimeStr() {
         return runningTimeStr;
     }
 
     public void setRunningTimeStr(String runningTimeStr) {
-        this.runningTimeStr.set(runningTimeStr);
+        this.runningTimeStr = runningTimeStr;
+        notifyPropertyChanged(BR.runningTimeStr);
     }
 
 
     public RunBasicInfoViewModel() {
-        this.distanceStr = new ObservableField<>();
-        this.paceStr = new ObservableField<>();
-        this.runningTimeStr = new ObservableField<>();
-
-        distanceStr.set("0.0");
-        paceStr.set("0\"0'");
-        runningTimeStr.set("00:00");
+        this.distanceStr = "0.0";
+        this.paceStr = "0\"0'";
+        this.runningTimeStr = "00:00";
     }
 }
