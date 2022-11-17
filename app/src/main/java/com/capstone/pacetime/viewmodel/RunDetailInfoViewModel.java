804ab49549cec26f2e3cb397ae9de976a55e88b9
girinssh <tngus5851@naver.com>
2022-11-18 02:54:26 +0900

    add data

--- app/src/main/java/com/capstone/pacetime/viewmodel/RunDetailInfoViewModel.java
+++ app/src/main/java/com/capstone/pacetime/viewmodel/RunDetailInfoViewModel.java
@@ -1,17 +1,57 @@
 package com.capstone.pacetime.viewmodel;
 
 import androidx.databinding.Bindable;
-import androidx.databinding.ObservableField;
 import androidx.databinding.library.baseAdapters.BR;
+
+import com.capstone.pacetime.RunInfo;
+import com.capstone.pacetime.data.Step;
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
+import java.util.Optional;
 
 public class RunDetailInfoViewModel extends RunBasicInfoViewModel{
     private String cadenceStr;
     private String stepCountStr;
+    private String startDateStr;
+    private String isBreathUsedStr;
+    private String startLocationStr;
 
     public RunDetailInfoViewModel() {
         super();
         cadenceStr = "0";
         stepCountStr = "0";
+        startLocationStr = "Dokdo";
+        // 시작 날짜 YYYY-MM-DD
+        startDateStr = OffsetDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        // isBreathUsed
+        isBreathUsedStr = "O";
+    }
+
+    public RunDetailInfoViewModel(RunInfo info){
+        cadenceStr = String.valueOf(info.getCadence());
+
+        stepCountStr = "-1";
+        info.getStepCount().stream().max(new Comparator<Step>() {
+            @Override
+            public int compare(Step o1, Step o2) {
+                if(o1.getCount() < o2.getCount()){
+                    return 1;
+                }else if(o1.getCount() > o2.getCount()){
+                    return -1;
+                }
+                return 0;
+            }
+        }).ifPresent(step -> stepCountStr = "" + step.getCount());
+
+        // 시작 장소
+        startLocationStr = "Dokdo";
+        // 시작 날짜 YYYY-MM-DD
+        startDateStr = info.getStartDateTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        // isBreathUsed
+        isBreathUsedStr = info.getIsBreathUsed() ? "O" : "X";
+        //
     }
 
     @Bindable
